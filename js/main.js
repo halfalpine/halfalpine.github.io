a74b1041d60ff4c0d0f35c99f141447acaa4aa34
halfalpine <rocksandwoks@gmail.com>
2016-09-27 19:12:19 -0400

    Inital change bg-color in contacts

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
 window.onload = function() {
   var squares = document.getElementsByClassName('contact');
-  var getSquares = function(){
-
-  };
+  // Applies pickColor to pickSquare
   var newColor = function() {
-    var squares = document.getElementsByClassName('contact');
+    var color = pickColor();
+    var s = pickSquare(squares.length);
+    s.style.backgroundColor="rgba(" + pickColor().join(',') + ", 1)";
+    console.log(s);
   };
-  var pickColor = function(prevColor){
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    return randomColor;
+  // Creates random RGB value
+  var pickColor = function(){
+    var randRed = random256();
+    var randGreen = random256(randRed);
+    var randBlue = random256(randBlue);
+    function random256(prevColor) {
+      var randomColor = Math.floor(Math.random()*255).toString(10);
+       return Math.abs(randomColor - prevColor) < 16 ? randomColor += 72 : randomColor;
+    }
+    return [randRed, randBlue, randGreen];
   };
+  // Returns a random element from the contact links
   var pickSquare = function(numSquares) {
-    var randomSquare = Math.floor(Math.random()* numSquares) + 1;
-    return randomSquare;
+    return squares[Math.floor(Math.random() * numSquares)];
   };
-  newColor();
   console.log('squares', squares);
   console.log('square1', squares[0]);
   console.log('square2', squares[1]);
   console.log('square3', squares[2]);
   console.log('square4', squares[3]);
-  window.setInterval(newColor, 5000);
+  console.log(pickSquare(squares.length));
+  console.log(pickSquare(squares.length));
+  console.log(pickSquare(squares.length));
+  console.log(pickSquare(squares.length));
+  setInterval(() => newColor(), 5000);
 };
