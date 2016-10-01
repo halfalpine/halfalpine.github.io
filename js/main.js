ce9b0cb0a58ddbb5fd5824d63f84e6f2f972dd79
halfalpine <rocksandwoks@gmail.com>
2016-10-01 10:39:48 -0400

    Initiate module architecture in main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
 window.onload = function() {
-  var squares = document.getElementsByClassName('contact');
-  // Applies pickColor to pickSquare
-  var newColor = function() {
-    var color = pickColor();
-    var square = pickSquare(squares.length);
-    square.style.backgroundColor="rgba(" + pickColor().join(',') + ", 1)";
+  var Colors = function() {
+    this.squares = document.getElementsByClassName('contact');
   };
-  // Creates random RGB value
-  var pickColor = function(){
-    var randRed = random256();
-    var randGreen = random256(randRed);
-    var randBlue = random256(randBlue);
-    function random256(prevColor) {
-      var randomColor = Math.floor(Math.random()*255).toString(10);
-       return Math.abs(randomColor - prevColor) < 16 ? randomColor += 72 : randomColor;
-    }
-    return [randRed, randBlue, randGreen];
+  Colors.prototype = {
+    init: function() {
+      Array.prototype.forEach.call(squares, (square) => {square.style.backgroundColor="rgba(" + pickColor().join(',') + ", 1)"});
+      setInterval(() => newColor(), 2000);
+    },
+    // Applies pickColor to pickSquare
+    newColor: function() {
+      // Remove?
+      // var color = pickColor();
+      var square = pickSquare(squares.length);
+      square.style.backgroundColor="rgba(" + pickColor().join(',') + ", 1)";
+    },
+    // Creates random RGB value
+    pickColor: function(){
+      var randRed = random256();
+      var randGreen = random256(randRed);
+      var randBlue = random256(randBlue);
+      function random256(prevColor) {
+        var randomColor = Math.floor(Math.random()*255).toString(10);
+        return Math.abs(randomColor - prevColor) < 16 ? randomColor += 72 : randomColor;
+      }
+      return [randRed, randBlue, randGreen];
+    },
+    // Returns a random element from the contact links
+    pickSquare: function(numSquares) {
+      return squares[Math.floor(Math.random() * numSquares)];
+    },
   };
-  // Returns a random element from the contact links
-  var pickSquare = function(numSquares) {
-    return squares[Math.floor(Math.random() * numSquares)];
-  };
-  // squares is an object, not an array
-  //squares.forEach((square) => console.log(square));
-  console.log(squares);
-  Array.prototype.forEach.call(squares, (square) => {square.style.backgroundColor="rgba(" + pickColor().join(',') + ", 1)"});
-  setInterval(() => newColor(), 2000);
+  Colors.init();
 };
